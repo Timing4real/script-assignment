4be6421b62c24af90b8e61826095872b98af8477
Timing4real <123456789+odin@users.noreply.rtcares2016@gmail.com>
2024-07-19 16:14:54 +0100

    create assignment on script

---
+++ main.js
@@ -0,0 +1,9 @@
+//Using psuedocode, write a function to output goodafternoon Rotimi,
+//where Rotimi is the value of the variable. Solution
+//define the function:
+function greetings(name) {
+let greetings = "Good afternoon!, ${name}";
+console.log("Good afternoon!, ${name}");
+};
+//initialize name thus:
+greetings('Rotimi');
---
+++ script.js
@@ -0,0 +1,12 @@
+//write a function to age and return the year you were born using psuedocode.
+//solution: define the function thus:
+function birth_year(currrent_year, age) {
+//hence, summon or invoke the current_year and input age respectively and, compute
+//the birth year as follows:
+  let birth_year = currrent_year - age;
+  //therfore,
+  console.log(birth_year);
+};
+//given 2024, as the current_year whilst 30 as the age, hence:
+birth_year(2024, 30);
+//conclusively, 1994 is therefore the birthyear.
