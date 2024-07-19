e4150ef52558b7707a00941ae95a60c8822ab970
Timing4real <123456789+odin@users.noreply.rtcares2016@gmail.com>
2024-07-19 22:38:28 +0100

    learning dom with js

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
+//DOM
+//let title = document.getElementById("title");
+//title.addEventListener("click", () => {
+//title.innerHTML = "I am learning DOM with js";
+//})
+
+
+
+
+
 //Using psuedocode, write a function to output goodafternoon Rotimi,
 //where Rotimi is the value of the variable. Solution
 //define the function:
-function greetings(name) {
-let greetings = "Good afternoon!, ${name}";
-console.log("Good afternoon!, ${name}");
+const greetings = (name) => {
+let message = `Good afternoon! ${name}`;
+console.log(message);
 };
 //initialize name thus:
 greetings('Rotimi');
+greetings('Liza');
--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 //write a function to age and return the year you were born using psuedocode.
 //solution: define the function thus:
-function birth_year(currrent_year, age) {
+function birth_year(age) => {
 //hence, summon or invoke the current_year and input age respectively and, compute
 //the birth year as follows:
-  let birth_year = currrent_year - age;
-  //therfore,
+let current_year = new Date().getFullYear();
+  let birth_year = current_year - age;
+  //therfore initialize birth_year,
   console.log(birth_year);
 };
 //given 2024, as the current_year whilst 30 as the age, hence:
-birth_year(2024, 30);
+birth_year(30);
 //conclusively, 1994 is therefore the birthyear.
